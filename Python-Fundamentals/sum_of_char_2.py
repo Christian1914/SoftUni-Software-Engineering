21581de1e55fef7bd440189358da7c3d101778af
Hristian Tuhlev <htuhlev88@gmail.com>
2022-05-30 19:46:46 +0300

    Add some codes

---
+++ Python-Fundamentals/sum_of_char_2.py
@@ -0,0 +1,8 @@
+start_index = int(input())
+final_index = int(input())
+final_string = ""
+
+for character in range(start_index, final_index +1):
+    final_string += chr(character) + " "
+
+print(final_string)
