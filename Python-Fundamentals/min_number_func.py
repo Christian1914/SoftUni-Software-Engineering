eaf125b5e5e736f608017f8981e32669ac1f1ae7
Hristian Tuhlev <htuhlev88@gmail.com>
2022-06-12 16:19:17 +0300

    task with functions added

---
+++ Python-Fundamentals/min_number_func.py
@@ -0,0 +1,18 @@
+# def min_number(first_number, second_number, third_number):
+#     return min(first_number, second_number, third_number)
+#
+# first_number = int(input())
+# second_number = int(input())
+# third_number = int(input())
+# print(min_number(first_number, second_number, third_number))
+
+def smallest_number(some_numbers):
+    return min(some_numbers)
+
+
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+all_numbers = [first_number, second_number, third_number]
+min_number = smallest_number(all_numbers)
+print(min_number)
