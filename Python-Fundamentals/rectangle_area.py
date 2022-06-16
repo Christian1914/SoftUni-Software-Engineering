75ab5b0cf0c14a0a20f3e78da2ca40f09714ed6e
Hristian Tuhlev <htuhlev88@gmail.com>
2022-06-16 19:14:37 +0300

    rectangle area with functions

---
+++ Python-Fundamentals/rectangle_area.py
@@ -0,0 +1,14 @@
+# def area(width, height):
+#     return width * height
+#
+# width = int(input())
+# height = int(input())
+# print(area(width, height))
+
+def min(first_number, second_number, third_number):
+    return min(first_number, second_number, third_number)
+
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+print(min())
