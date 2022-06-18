8d842bc8c905bd45ade698d04c66dbde06fde92b
Hristian Tuhlev <htuhlev88@gmail.com>
2022-06-18 23:07:18 +0300

    new tasks

---
+++ Python-Fundamentals/double_charm.py
@@ -0,0 +1,11 @@
+string = input()
+while string != "End" :
+    if string != "SoftUni" :
+        for x in string :
+            print(x,end = "")
+            print(x,end = "")
+        #for x in range(len(string)+1) :
+         #   print(string[x-1:x:], end = "")
+          #  print(string[x - 1:x:], end="")
+        print()
+    string = input()
