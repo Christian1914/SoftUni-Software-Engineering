916a40ba0b062c87b5db7b376eb166c150547801
Hristian Tuhlev <htuhlev88@gmail.com>
2022-06-19 23:32:52 +0300

    functions demo

---
+++ Python-Fundamentals/functions.py
@@ -0,0 +1,34 @@
+# def odd_even_num(number):
+#     result = None
+#     if number % 2 == 0:
+#         result = "Even"
+#     else:
+#         result = "Odd"
+#
+#     return result
+#
+# print(odd_even_num(6))
+
+# def grade(current_grade):
+#     result = None
+#     if 2.00 <= current_grade <= 2.99:
+#         result = "Fail"
+#     elif 3.00 <= current_grade <= 3.49:
+#         result = "Poor"
+#     elif 3.50 <= current_grade <= 4.49:
+#         result = "Good"
+#     elif 4.50 <= current_grade <= 5.49:
+#         result = "Very Good"
+#     elif 5.50 <= current_grade <= 6.00:
+#         result = "Excellent"
+#
+#
+#     return result
+# score = float(input())
+# print(grade(score))
+
+def f1():
+    x = 100
+    return x
+x = f1() + 1
+print(x)
