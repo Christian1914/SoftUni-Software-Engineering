0806ecaf5e3160e92f2b7023bf5f2845850a1c41
Hristian Tuhlev <htuhlev88@gmail.com>
2022-10-07 15:10:36 +0300

    string and functions

---
+++ JavaScript-Fundamentals/Functions/repeatStringFunction.js
@@ -0,0 +1,16 @@
+function repeatString(text, count){
+    //text.repeat(count)
+
+
+
+
+    //let result = '';
+
+    //for(let i = 0; i < count; i++){
+        //result += text
+
+    //}
+    console.log(text.repeat(count))
+
+}
+repeatString("abc", 3);
