488a0ea4e9c56b5e78a082bbc8c734e97a448e2a
Hristian Tuhlev <htuhlev88@gmail.com>
2022-10-09 17:30:44 +0300

    new school task with functions

---
+++ JavaScript-Fundamentals/Functions/addAndSubtract.js
@@ -0,0 +1,14 @@
+function allInOne(numOne, numTwo, numThree){
+    let sumup = sum(numOne, numTwo, numThree)
+    let allsum = subtract(numOne, numTwo, numThree)
+
+
+function sum(numOne, numTwo, numThree){
+    return numOne + numTwo
+}
+
+function subtract(numOne, numTwo, numThree){
+    return sumup - numThree
+}
+console.log(allsum);
+}
