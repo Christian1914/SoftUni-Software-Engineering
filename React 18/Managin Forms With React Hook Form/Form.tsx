dfae69c777729b45b730524560c836bfe7c31a9f
Hristian Tuhlev <htuhlev88@gmail.com>
2023-05-30 12:24:14 +0300

    react hook form

---
+++ React 18/Managin Forms With React Hook Form/Form.tsx
@@ -0,0 +1,61 @@
+import React, { FormEvent, useRef, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
+interface FormData {
+  name: string;
+  age: number;
+}
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm <FormData>();
+  
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          {...register("name", { required: true, minLength: 3, maxLength: 10 })}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required.</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">The name must be at least 3 characters.</p>
+        )}
+        {errors.name?.type === "maxLength" && (
+          <p className="text-danger">
+            The name must be shorter than 10 characters.
+          </p>
+        )}
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          {...register("age")}
+          id="age"
+          type="number"
+          className="form-control"
+        />
+      </div>
+
+      <button className="btn btn-dark" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
