ad23a034e46d581414b011ea5d62137ead9bd33d
Hristian Tuhlev <htuhlev88@gmail.com>
2023-12-23 00:36:34 +0200

    adding smooth scrolling and button to go to top

--- HTML & CSS/Omnifood project/script.js
+++ HTML & CSS/Omnifood project/script.js
@@ -16,35 +16,94 @@ const headerEl = document.querySelector('.header');
 
 
 btnNavEl.addEventListener('click', function () {
-    // The keyword here is toggle, if we make it with add it will only work one way
-    headerEl.classList.toggle('nav-open')
+  // The keyword here is toggle, if we make it with add it will only work one way
+  headerEl.classList.toggle('nav-open')
 })
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const href = link.getAttribute('href');
+
+    // Scroll back to top
+    if (href === '#') window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle('nav-open')
+    }
+
+  })
+})
+
+
+///////////////////////////////////////////////////////////
+// Scroll to top button
+
+document.addEventListener('DOMContentLoaded', function () {
+  let scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+  window.addEventListener('scroll', function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopBtn.style.display = 'block';
+    } else {
+      scrollToTopBtn.style.display = 'none';
+    }
+  });
+
+  scrollToTopBtn.addEventListener("click", function () {
+    // Use JavaScript to achieve smooth scrolling
+    let scrollOptions = {
+      top: 0,
+      behavior: "smooth"
+    };
+    window.scrollTo(scrollOptions);
+  });
+});
+
+
+
 
 
 ///////////////////////////////////////////////////////////
 // Set current year
 document.addEventListener("DOMContentLoaded", function () {
-    var currentYear = new Date().getFullYear();
-    document.getElementById("currentYear").innerText = currentYear;
+  var currentYear = new Date().getFullYear();
+  document.getElementById("currentYear").innerText = currentYear;
 });
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
-    var flex = document.createElement("div");
-    flex.style.display = "flex";
-    flex.style.flexDirection = "column";
-    flex.style.rowGap = "1px";
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
 
-    flex.appendChild(document.createElement("div"));
-    flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
 
-    document.body.appendChild(flex);
-    var isSupported = flex.scrollHeight === 1;
-    flex.parentNode.removeChild(flex);
-    console.log(isSupported);
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
 
-    if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
 checkFlexGap();
 
