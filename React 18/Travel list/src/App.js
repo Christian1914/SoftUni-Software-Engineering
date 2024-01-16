1b5d3e90a6f9c78f8162af89ebac21bc821a5408
Hristian Tuhlev <htuhlev88@gmail.com>
2024-01-16 15:43:05 +0200

    updating code

--- React 18/Travel list/src/App.js
+++ React 18/Travel list/src/App.js
@@ -13,9 +13,7 @@ function App() {
     setItems((prevItems) => [...prevItems, item]);
   }
 
-  function handleRemoveItem(id) {
-    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
-  }
+  
 
   return (
     <div className="app">
